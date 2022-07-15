601a5fb4ac9478b1287131eb97e7035a59736f42
stevforgit <srodr@sertiscorp.com>
2022-07-16 06:30:20 +0700

    scripts upated

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app-new
+sudo chmod -R 777 /home/ec2-user/express-app
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app-new
+cd /home/ec2-user/express-app
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
@@ -15,7 +15,7 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #Build app
-npm run build
+# npm run build
 
 #start our node app in the background
-#node app.js > app.out.log 2> app.err.log < /dev/null & 
+node app.js > app.out.log 2> app.err.log < /dev/null & 
