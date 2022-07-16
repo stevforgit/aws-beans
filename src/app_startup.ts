de1a881f9668e82b2796a00f569a9979af06a9ac
stevforgit <srodr@sertiscorp.com>
2022-07-16 07:43:30 +0700

    root url updated

--- src/app_startup.ts
+++ src/app_startup.ts
@@ -1,9 +1,13 @@
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { v1 } from './modules/routes/v1/index';
 import { v2 } from './modules/routes/v2/index';
 
-const app: any = express();
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('healthy');
+})
 
 app.use('/api/v1', v1);
 app.use('/api/v2', v2);
